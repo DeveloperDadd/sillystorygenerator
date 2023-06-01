49701f203cefc6642bff477cf28e861b83076487
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-06-01 01:08:26 +0000

    added replace method to newStory variable to replace temperatures and weights when uk radio button is selected

--- main.js
+++ main.js
@@ -40,10 +40,10 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300/14) + ' stone';
-    const temperature =  Math.round((94 - 32) * 5 / 9) + ' centigrade';
-    newStory.replace('94 fahrenheit', temperature);
-    newStory.replace('300 pounds', weight);
+    const weight = Math.round(300/14) + " stone";
+    const temperature =  Math.round((94 - 32) * 5 / 9) + " centigrade";
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
   }
 
   story.textContent = newStory;
