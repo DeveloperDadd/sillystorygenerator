a2d440d41af74399d2cb2baec338d382fb2f5386
Clayton Critchfield <clayton.critchfield@gmail.com>
2023-06-01 01:04:55 +0000

    fixed case sensitivity issue with :inserty: in the storyText variable, from capital Y to lowercase y

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function randomValueFromArray(array){
 
 // RAW TEXT STRINGS
 
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :insertY:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
 const insertX = ["Willy the Goblin","Big Daddy","Father Christmas"];
 
